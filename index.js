c1985894dd96cf7b492c79ff632e20447b033bcc
Oscar <oscarkimani86@icloud.com>
2024-04-16 08:18:05 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(list, name) {
+    return list.filter (element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch (list, str) {
+    const size = str.length;
+    return list.filter (name => name.slice(0, size) === str);
+}
+
+function matchName(list, str) {
+    return list.filter(obj => obj.name === str);
+}
